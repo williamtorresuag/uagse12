5daf675b0f9f96ba319603ff5b6a076dee331eda
dopeezy <williamtorres@uagateway.org>
2016-10-24 17:47:45 +0000

    scanner

---
+++ practice/practiceInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class practiceInput{
+
+public static void main(String[] args){
+Scanner in = new Scanner(System.in);
+System.out.print("Whats your name? ");
+String s = in.nextLine();
+System.out.println("whats up, " + s);
+
+ }
+}
